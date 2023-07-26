41478401e59af4da9c9e9b4db75a893a23ffcd3f
CheatSetha <cheatsetha2001@gmail.com>
2023-07-27 00:59:05 +0700

    up to date

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import PreviewALl from "./views/DesignEditor/components/Preview/TestPreview"
 
 import DesignEditorWatermak from "./views/DesignEditor/WatermarkRoute"
 import DesignEditorCertificate from "./views/DesignEditor/Certificate"
+import Skeleton from "./components/Loading/skeleton/Skeleton"
 
 
 const Router = () => {
@@ -13,7 +14,7 @@ const Router = () => {
       <Routes>
         <Route path="/manage" element={<Dashboard />} />
         <Route path="/" element={<DesignEditor />} />
-        {/* <Route path="/previewall" element={<PreviewALl />} /> */}
+        <Route path="/previewall" element={<Skeleton />} />
         <Route path="/watermark" element={<DesignEditorWatermak />} />
         <Route path="/generatecertificate" element={<DesignEditorCertificate/>} />
 
---
+++ src/components/Loading/skeleton/Skeleton.tsx
@@ -0,0 +1,9 @@
+export default function Skeleton() {
+  return (
+    <div>
+    
+        <div className="skeleton bg-blue-500 h-24"></div>
+       
+    </div>
+  )
+}
